cea6f1d4cfdb9167b33b9ac549599d93f0649c25
Jesús Espino <jesus.espino@kaleidos.net>
2014-04-27 23:52:33 +0200

    Work in progress

--- foobadges-cli.py
+++ foobadges-cli.py
@@ -1,17 +1,58 @@
 from pymongo import MongoClient
 import settings
 from cmd import Cmd
+import sys
 
 client = MongoClient(settings.MONGO_HOST, settings.MONGO_PORT)
 db = client[settings.MONGO_DB]
 
 
 class FooBadgesCmdClient(Cmd):
+    def do_EOF(self, args):
+        sys.exit()
+
+    def do_exit(self, args):
+        sys.exit()
+
     def do_revoke(self, args):
-        print(args)
         assertion_id = input("Assertion id: ")
         reason = input("Reason: ")
         db.revocations.insert({assertion_id: reason})
+
+    def do_new_badge(self, args):
+        slug = input("slug: ")
+        title = input("Title: ")
+        description = input("Description: ")
+        image = input("Image url: ")
+        criteria = input("Criteria: ")
+
+        num_of_alignments = input("Number of alignments: ")
+        alignments = []
+        for x in range(int(num_of_alignments)):
+            alignment_name = input("Alignment %s name: " % x)
+            alignment_url = input("Alignment %s url: " % x)
+            alignment_description = input("Alignment %s description: " % x)
+            alignments.append({
+                "name": alignment_name,
+                "url": alignment_url,
+                "description": alignment_description,
+            })
+
+        tags = []
+        num_of_tags = input("Number of tags: ")
+        for x in range(int(num_of_tags)):
+            tags.append(input("Tag: "))
+
+        db.badges.insert({
+            "_id": slug,
+            "title": title,
+            "description": description,
+            "image": image,
+            "criteria": criteria,
+            "alignments": alignments,
+            "tags": tags,
+            "issuer": "/issuer"
+        })
 
 if __name__ == '__main__':
     cmd = FooBadgesCmdClient()
--- service.py
+++ service.py
@@ -12,7 +12,7 @@ def get_assertion_by_id(assertion_id):
     return db.assertions.find_one({"_id": assertion_id})
 
 def get_badge_by_slug(slug):
-    return db.badges.find_one({"_id": slug})
+    return db.badges.find_one({"_id": slug}, fields={"_id": False})
 
 def get_criterion_by_slug(slug):
     return db.criterions.find_one({"_id": slug})
