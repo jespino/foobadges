4e9489b8cf1d3b640bfd88f856b09ad200a6591f
Jesús Espino <jesus.espino@kaleidos.net>
2014-05-02 23:13:55 +0200

    Now alwais response are application/json content type

--- foobadges.py
+++ foobadges.py
@@ -1,7 +1,6 @@
 import settings
 import json
-from flask import Flask
-from flask import request
+from flask import Flask, request, Response
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -16,27 +15,27 @@ def home():
 @app.route("/revoked")
 def revoked():
     cursor = db.revocations.find(fields={"_id": False})
-    return json.dumps(list(cursor))
+    return Response(json.dumps(list(cursor)), content_type="application/json")
 
 @app.route("/issuer")
 def issuer():
-    return json.dumps({
+    return Response(json.dumps({
         'name': settings.ISSUER_NAME,
         'image': settings.ISSUER_IMAGE_URL,
         'url': settings.ISSUER_URL,
         'email': settings.ISSUER_EMAIL,
         'revocationList': request.url_root + "revoked"
-    })
+    }), content_type="application/json")
 
 @app.route("/assertion/<assertion_id>")
 def assertion(assertion_id):
     assertion = db.assertions.find_one({"_id": assertion_id}, fields={"_id": False})
-    return json.dumps(assertion)
+    return Response(json.dumps(assertion), content_type="application/json")
 
 @app.route("/badge/<badge_slug>")
 def badge(badge_slug):
     badge = db.badges.find_one({"_id": badge_slug}, fields={"_id": False})
-    return json.dumps(badge)
+    return Response(json.dumps(badge), content_type="application/json")
 
 if __name__ == '__main__':
     app.run(debug=True)
